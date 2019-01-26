554ec03d7ffe906dee23778aea648400210e1af7
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2019-01-26 13:26:40 +0100

    Update nqueen.cpp

--- cpp/nqueen.cpp
+++ cpp/nqueen.cpp
@@ -5,10 +5,10 @@
 // @Version: 1
 // @Purpose: solve the n-queeen problem with the depth-first search
 //
-// Compilation:
+// @Compilation:
 // g++ xxx.cpp -std=c++0x -Wall -O -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused
 //
-/* Exemple class call Nqueen to solve the problem n-queen:
+/* @Exemple class call Nqueen to solve the problem n-queen:
 using namespace jxtopher;
 
 int main() {
