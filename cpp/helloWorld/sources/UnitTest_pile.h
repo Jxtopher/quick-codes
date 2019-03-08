f5d6ba5754a7bc4867e693db2a534ed31e11548f
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2019-03-08 10:56:02 +0100

    Add files via upload

---
+++ cpp/helloWorld/sources/UnitTest_pile.h
@@ -0,0 +1,64 @@
+//
+// @Author: *
+// @License: *
+// @Date: *
+// @Version: *
+// @Purpose: Hello world for a simple exemple code
+// 
+
+#ifndef JXTOPHER_UNITTEST_PILE_H
+#define JXTOPHER_UNITTEST_PILE_H
+
+#include <iostream>
+#include <string>
+#include <list>
+
+#include "pile.h"
+
+using namespace std;
+using namespace CppUnit;
+using namespace jxtopher;
+
+
+class UnitTest_pile : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(UnitTest_pile);
+    CPPUNIT_TEST(test_push);
+    CPPUNIT_TEST(test_operatoreqeq);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp(void) {
+        p = new Pile<unsigned int>;
+    }
+    void tearDown(void) {
+        delete p;
+    }
+
+protected:
+    void test_push(void) {
+        unsigned int a = 5;
+        p->push(a);
+        CPPUNIT_ASSERT(p->isEmpty() == false);
+
+        unsigned int b = p->pop();
+        CPPUNIT_ASSERT(b == a);
+    }
+    void test_operatoreqeq(void) {
+        Pile<unsigned int> p1;
+        
+
+        for (unsigned int i = 0 ; i < 100 ; i++)
+            p1.push(i);
+
+        Pile<unsigned int> p2(p1);
+        CPPUNIT_ASSERT(p2 == p1);
+        for (unsigned int i = 100 ; 0 < i  ; i--)
+            CPPUNIT_ASSERT(p1.pop() == p2.pop());
+
+    }
+
+private:
+    Pile<unsigned int> *p;
+};
+
+#endif
---
+++ cpp/helloWorld/sources/pile.h
@@ -0,0 +1,121 @@
+//
+// @Author: *
+// @License: *
+// @Date: *
+// @Version: *
+// @Purpose: 
+//          Définir un class Pile avec les fonction push, pop, isEmpty, size
+//          Mettre en place une gestion dynamique de la mémoire
+//          Définir les constructeurs de copie et l'opérateur égale
+//
+
+#ifndef JXTOPHER_PILE_H
+#define JXTOPHER_PILE_H
+
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+
+#include "macro.h"
+
+using namespace std;
+
+namespace jxtopher {
+
+template<class TYPE>
+class Pile {
+    public:
+    Pile() {
+        data = new TYPE[0];
+        nbElementMax = 0;
+        nbElement = 0;
+        delta = 30;
+    }
+
+    virtual ~Pile() {
+        delete data;
+    }
+
+    Pile(Pile const &p) {
+        nbElementMax = p.nbElementMax;
+        nbElement = p.nbElement;
+        delta = p.delta;
+        data = new TYPE[nbElementMax];
+        for (unsigned int i = 0 ; i < nbElementMax ; i++) {
+            data[i] = p.data[i];
+        }
+    }
+
+    Pile & operator=(const Pile  &p) {
+        if (nbElementMax != p.nbElementMax)
+            data = static_cast<TYPE *>( realloc (data, p.nbElementMax * sizeof(TYPE)));
+       
+        nbElementMax = p.nbElementMax;
+        nbElement = p.nbElement;
+        delta = p.delta;
+        for (unsigned int i = 0 ; i < nbElementMax ; i++) {
+            data[i] = p.data[i];
+        }
+    }
+
+    bool operator==(const Pile  &p) const {
+        if (p.size() != size())
+            return false;
+        
+        for (unsigned int i = 0 ; i < size() ; i++) {
+            if (p.data[i] != data[i])
+                return false;
+        }
+        return true;
+    }
+
+    bool operator!=(const Pile &p) const {
+        return not(operator==(p));
+    }
+
+    void push(TYPE element) {
+        if (nbElement == nbElementMax) {
+            nbElementMax += 10;
+            data = static_cast<TYPE *>( realloc (data, nbElementMax * sizeof(TYPE)));
+            data[nbElement] = element;
+            nbElement++;
+        } else {
+            data[nbElement] = element;
+            nbElement++;
+        }
+    }
+
+    TYPE pop() {
+        if (nbElement == 0) {
+            THROW("nbElement == 0")
+        }
+        if (delta < (nbElementMax - nbElement)) {
+            // Liberation de la mémoire
+            nbElementMax -= 10;
+            data = static_cast<TYPE *>( realloc (data, nbElementMax * sizeof(TYPE)));
+        }
+        nbElement--;
+        return data[nbElement];
+    }
+
+    bool isEmpty() const {
+        if (nbElement == 0)
+            return true;
+        else 
+            return false;
+    }
+
+    unsigned int size() const {
+        return nbElement;
+    }
+
+    protected:
+    unsigned int nbElementMax;
+    unsigned int nbElement;
+    unsigned int delta;
+    TYPE *data;
+};
+
+#endif
+
+}
